42604c881e3f4a88f33a7fbb9dc7db9bed829e9a
Johan Sierra <johansierra553@gmail.com>
2023-09-27 21:02:49 -0500

    Modificando la lista de contatos para que guarde objetos

--- contactos .js
+++ contactos .js
@@ -1,4 +1,33 @@
-const contactos = ["Johan Sierra", "Hanne Sierra", "Marcela Corredor", "Miguel Sierra"]
+const contactos = [
+    {
+        id: 1,
+        nombres: "Johan",
+        apellidos: "Sierra",
+        teléfono: 324672456,
+        ubicaciones: [ "Bogota", "Cr 24 #5-07sur"]
+    },
+    {
+        id: 2,
+        nombres: "Hanne",
+        apellidos: "Sierra",
+        teléfono: 321024674,
+        ubicaciones: [ "Bogota", "Carreara 12 #12-5"]
+    },
+    {
+        id: 3,
+        nombres: "Marcela",
+        apellidos: "Corredor",
+        teléfono: 320939386,
+        ubicaciones: [ "Bogota", "Calle 24f #13-32"]
+    },
+    {
+        id: 1,
+        nombres: "Miguel",
+        apellidos: "Lopez",
+        teléfono: 319274918,
+        ubicaciones: [ "Bogota", "Cl 77 #5-07"]
+    }
+]
 
 
 // Funcion para crear un nuevo contacto
