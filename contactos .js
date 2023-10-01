7aa6a798f7cda8fc734cafe8eb011d608aacf340
Johan Sierra <johansierra553@gmail.com>
2023-10-01 02:29:36 -0500

    Mejorando y corrigiendo el codigo

--- contactos .js
+++ contactos .js
@@ -1,29 +1,29 @@
 const contactos = [
     {
         id: 1,
-        nombres: "Johan",
-        apellidos: "Sierra",
+        nombre: "Johan",
+        apellido: "Sierra",
         telefono: 324672456,
         ubicacion: [ "Bogota", "Cr 24 #5-07sur"]
     },
     {
         id: 2,
-        nombres: "Hanne",
-        apellidos: "Sierra",
+        nombre: "Hanne",
+        apellido: "Sierra",
         telefono: 321024674,
         ubicacion: [ "Bogota", "Carreara 12 #12-5"]
     },
     {
         id: 3,
-        nombres: "Marcela",
-        apellidos: "Corredor",
+        nombre: "Marcela",
+        apellido: "Corredor",
         telefono: 320939386,
         ubicacion: [ "Bogota", "Calle 24f #13-32"]
     },
     {
         id: 4,
-        nombres: "Miguel",
-        apellidos: "Lopez",
+        nombre: "Miguel",
+        apellido: "Lopez",
         telefono: 319274918,
         ubicacion: [ "Bogota", "Cl 77 #5-07"]
     }
@@ -36,8 +36,8 @@ function newContact(id, nombre, apelllido, telefono, ciudad, direccion) {
     contactos.push(
         {
             id: id,
-            nombres: nombre,
-            apellidos: apelllido,
+            nombre: nombre,
+            apellido: apelllido,
             teléfono: telefono,
             ubicaciones: [ ciudad , direccion]
         }
@@ -50,7 +50,7 @@ newContact(5,"Jonnathan", "Sierra", 301238913, "Bogota", "Calle 32 #19-9")
 // Funcion para eliminar un contacto
 
 function deleteContact(contactoAEliminar) {
-    let contactoEliminado = contactos.filter(contacto => contacto.nombres == contactoAEliminar)
+    let contactoEliminado = contactos.filter(contacto => contacto.nombre == contactoAEliminar)
     console.log("Vas a eliminar este contacto", contactoEliminado)
     
 // Este bloque de codigo le da la opcion al usuario si quiere eliminar el contacto o no
@@ -62,6 +62,8 @@ function deleteContact(contactoAEliminar) {
     // } else {
     //     console.log("No has eliminado nigun contacto")
     // }
+
+    contactos.splice(contactoEliminado, 1)
 }
 deleteContact("Johan")
 
@@ -69,6 +71,7 @@ deleteContact("Johan")
 // Funcion para imprimir un contacto un nuevo contacto
 
 function printContact() {
+    console.log("Lista de contactos actual")
     for (let i = 0; i < contactos.length; i++) {
         const element = contactos[i];
         console.log(element)
@@ -84,4 +87,4 @@ function update(id, propiedad, actualizar) {
     contactos[actualiza][propiedad] = actualizar
     console.log(`Has actualizado al contacto con el ID: ${id}`, contactos[actualiza])
 }
-update(2, "nombres", "Camila")
+update(2, "nombre", "Camila")
